a6bca1b0b2c86e3a713814c9a9cbd075ff01e9f3
Isaac Parker <128327439+isaacparker0@users.noreply.github.com>
2026-02-19 10:00:03 -0500

    Clean

--- tools/bazel/workspace_status.sh
+++ tools/bazel/workspace_status.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if git_sha="$(git rev-parse HEAD 2>/dev/null)"; then
-	echo "STABLE_COMMIT_SHA ${git_sha}"
-else
-	echo "STABLE_COMMIT_SHA unknown"
-fi
+# Bazel workspace status command
+# https://bazel.build/docs/user-manual#workspace-status
+
+commit_sha=$(git rev-parse HEAD)
+echo "STABLE_COMMIT_SHA $commit_sha"
