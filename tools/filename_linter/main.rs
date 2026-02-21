c7fe3ddeac52f3a09e67781874524a9ff80b4883
Isaac Parker <128327439+isaacparker0@users.noreply.github.com>
2026-02-20 22:14:16 -0500

    0

--- tools/filename_linter/main.rs
+++ tools/filename_linter/main.rs
@@ -56,6 +56,7 @@ const RULES: &[Rule] = &[
         },
         exclude_patterns: Some(&[
             // keep-sorted start
+            ".buildkite/hooks/**",
             ".pre-commit-config.yaml",
             "Cargo.lock",
             "Cargo.toml",
