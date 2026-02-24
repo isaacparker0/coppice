afed20582a5359b65b55153962a7717c743ccfea
Isaac Parker <128327439+isaacparker0@users.noreply.github.com>
2026-02-24 18:27:35 -0500

    Github: delete branch on merge

--- terraform/github/main.tf
+++ terraform/github/main.tf
@@ -1,9 +1,12 @@
 resource "github_repository" "coppice" {
   name       = "coppice"
-  visibility = "private"
+  visibility = "public"
 
   allow_merge_commit          = false
   allow_rebase_merge          = false
+  delete_branch_on_merge      = true
+  merge_commit_message        = "BLANK"
+  merge_commit_title          = "PR_TITLE"
   squash_merge_commit_message = "BLANK"
   squash_merge_commit_title   = "PR_TITLE"
 }
